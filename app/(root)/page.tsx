f19981cea361c8b81f77c3ca1ba525778c3e474d
abhijay kheechee <abhijaysingh2003@gmail.com>
2024-09-03 00:54:07 -0400

    update Home page and add HeaderBox component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,20 @@
+import HeaderBox from "@/components/ui/HeaderBox";
+
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Abhijay" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions"
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 export default Home;
