d1e2981d07044fbb539140b16f9c50b297d638b4
abhijay kheechee <abhijaysingh2003@gmail.com>
2024-09-09 23:35:06 -0400

    add sidebar component and routes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import Sidebar from "@/components/ui/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Abhijay", lastName: "Kheechee" };
   return (
-    <main>
-      sidebar
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
       {children}
     </main>
   );
